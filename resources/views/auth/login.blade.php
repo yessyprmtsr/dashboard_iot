e6c5410c6df022672742e0886c6dfbcf9da4af1b
yessyprmtsrtmp <yessypermatasari8@gmail.com>
2022-01-19 19:41:03 +0700

    fix views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,8 +53,18 @@
                                                 id="exampleInputPassword" placeholder="Password" name="password">
                                         </div>
                                         <button class="btn btn-primary btn-user btn-block">Login</button>
+                                        {{-- <hr>
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a> --}}
                                     </form>
                                     <hr>
+                                    {{-- <div class="text-center">
+                                        <a class="small" href="register.html">Create an Account!</a>
+                                    </div> --}}
                                 </div>
                             </div>
                         </div>
--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -14,7 +14,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Hello</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
                 <img class="img-profile rounded-circle"
                     src="https://www.kindpng.com/picc/m/357-3575561_girl-young-beautiful-people-female-person-woman-user.png">
             </a>
